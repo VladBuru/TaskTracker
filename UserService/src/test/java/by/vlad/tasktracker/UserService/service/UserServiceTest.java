5377efd937cf66a0e324469e88439bb2959a7f2b
Vlad <akonnbaaaa@gmail.com>
2024-08-21 23:18:42 +0300

    написал пару тестов

--- UserService/src/test/java/by/vlad/tasktracker/UserService/service/UserServiceTest.java
+++ UserService/src/test/java/by/vlad/tasktracker/UserService/service/UserServiceTest.java
@@ -3,6 +3,7 @@ package by.vlad.tasktracker.UserService.service;
 import by.vlad.tasktracker.UserService.model.User;
 import by.vlad.tasktracker.UserService.repository.UserRepository;
 import by.vlad.tasktracker.UserService.util.exception.EmailAlreadyExistsException;
+import by.vlad.tasktracker.UserService.util.exception.UserNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -11,6 +12,7 @@ import org.mockito.MockitoAnnotations;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -48,6 +50,7 @@ public class UserServiceTest {
         when(userRepository.existsByEmail(email)).thenReturn(true);
 
         assertThrows(EmailAlreadyExistsException.class, () -> userService.createUser(user));
+
         verify(userRepository, never()).save(user);
     }
 
@@ -63,5 +66,25 @@ public class UserServiceTest {
         assertEquals(2, returnedUsers.size());
         assertEquals(user1, returnedUsers.get(0));
         assertEquals(user2, returnedUsers.get(1));
+    }
+
+    @Test
+    public void testGetUserById() {
+        User user = new User(1L,"user", "user1@test.com");
+        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+
+        User foundUser = userService.getUserById(1L);
+
+        assertEquals(user.getId(), foundUser.getId());
+        assertEquals(user.getName(), foundUser.getName());
+        assertEquals(user.getEmail(), foundUser.getEmail());
+    }
+
+    @Test
+    public void testGetUserByIdThrowsExceptionUserNotFoundException() {
+        Long notExistentId = 1L;
+        when(userRepository.findById(notExistentId)).thenReturn(Optional.empty());
+
+        assertThrows(UserNotFoundException.class, () -> userService.getUserById(1L));
     }
 }
